d317d71a004d9bed3af00a0c0f98a01f386fc0d4
Karl Cossio <karl@kymerastudio.com>
2023-05-14 15:07:23 -0700

    asdhn

--- assets/animation.js
+++ assets/animation.js
@@ -444,12 +444,14 @@ var slickSliders = function(){
      // Twinkle Stars function
 
      window.twinkle =function(container, amount) {
-        var vw = window.innerWidth;
-        var vh = window.innerHeight;
+        
 
         var textures = document.querySelectorAll(container + " .star");
         var main = document.querySelector(container);
         var frag = document.createDocumentFragment();
+
+        var vw = main.offsetWidth;
+        var vh = main.offsetHeight;
 
         var appearMin = 0.3;
         var appearMax = 0.8;
