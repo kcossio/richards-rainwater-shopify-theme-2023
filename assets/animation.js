32ef8e189f218c87285e476977270a50b5b3845b
Karl Cossio <karl@kymerastudio.com>
2023-05-14 16:38:39 -0700

    animmmaaate

--- assets/animation.js
+++ assets/animation.js
@@ -44,7 +44,7 @@
         }).progress(1);
 
         ScrollTrigger.create({
-        start: "top -10px",
+        start: "top -80px",
         end: 99999,
         onUpdate: (self) => {
         self.direction === -1 ? showAnim.play() : showAnim.reverse()
