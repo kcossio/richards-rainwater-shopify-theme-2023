444c9ed47a5957d399ebca0a27079ca82a7f55e1
Karl Cossio <karl@kymerastudio.com>
2023-05-14 15:20:59 -0700

    duur

--- assets/animation.js
+++ assets/animation.js
@@ -515,8 +515,8 @@ var slickSliders = function(){
             var duration1 = random(durationMin, durationMax);
             var duration2 = random(durationMin, durationMax);   
             
-            tl.set(star, {yPercent: 0}).to(star, duration1, { autoAlpha: alpha, scale: scale, ease: ease1 })
-                .to(star, duration2, { autoAlpha: 0, scale: 0, yPercent: 200, ease: ease2 }, appear)
+            tl.set(star, {yPercent: 0}).to(star, duration1, { autoAlpha: alpha, scale: scale, ease: none })
+                .to(star, duration1, { autoAlpha: 0, scale: 0, yPercent: 200, ease: none })
             }
             
             tl.progress(random(1));
