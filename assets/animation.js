521f74d4e10072a53afaf52aa76f6d17e07c3d1e
Karl Cossio <karl@kymerastudio.com>
2023-05-15 02:58:32 -0700

    scrrrollmeee

--- assets/animation.js
+++ assets/animation.js
@@ -558,6 +558,15 @@ var slickSliders = function(){
         }
     }
 
+    jQuery('a.scrollme').click(function() { 
+        var target = jQuery(this.hash);
+        if (target.length == 0) target = jQuery('a[name="' + this.hash.substr(1) + '"]');
+        if (target.length == 0) target = jQuery('html');
+        jQuery('html, body').delay(100).animate({ scrollTop: target.offset().top - 0 }, 500);
+        ScrollTrigger.refresh();
+        return false;
+    });
+
    
 
 
