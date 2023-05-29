5a651f53a20b243c36c1546b96d2e444c380d754
Karl Cossio <karl@kymerastudio.com>
2023-05-29 07:56:56 -0700

    asdg

--- assets/animation.js
+++ assets/animation.js
@@ -112,6 +112,7 @@
         });
         //Destini Reveal on Load
         var destini_tl = gsap.timeline({repeat: 0, delay: 1.5});
+        destini_tl.to(".destini-inner span", { opacity: 0, scale: 0 });
         destini_tl.to(".destini-inner .overlay", {   
             height: 0,
             ease: "easeInOut",
