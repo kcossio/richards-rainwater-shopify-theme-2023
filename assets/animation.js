2265345945f09b8821f5cd001204e3d511104623
Karl Cossio <karl@kymerastudio.com>
2023-08-02 18:04:42 -0700

    Update animation.js

--- assets/animation.js
+++ assets/animation.js
@@ -3,7 +3,7 @@
     // Add spacing if a hashtag is in the url 
     function offsetAnchor() {
         if (location.hash.length !== 0) {
-            window.scrollTo(window.scrollX, window.scrollY - 100);
+            window.scrollTo(window.scrollX, window.scrollY - 120);
         }
     }
 
